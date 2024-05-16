33980622e7ecffe45505717f3440c446514a77cf
selselx <selvaartunc@gmail.com>
2024-05-16 17:21:08 +0300

    ig its right this time

---
+++ formproject/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <singupform/>
+</template>
+
+<script>
+
+import singupform from "./components/singupform.vue"
+
+export default {
+  name: 'App',
+  components:{singupform}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  background:black;
+}
+</style>
---
+++ formproject/src/components/singupform.vue
@@ -0,0 +1,36 @@
+<template>
+  <form>
+    <label >Email:</label>
+    <input type="Email" required v-model="email">
+    <label >Password:</label>
+    <input type="Email" required v-model="password">
+    <label >Role:</label>
+   <select v-model="role">
+    <option value="developer">Web Developer</option>
+    <option value="designer">Web Designer</option>
+   </select>
+   <div class="terms">
+   <input type="checkbox" required>
+   <label>Accept terms and conditions</label>
+   </div>
+  </form>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        email:'',
+        password:'',
+        role:'',
+        terms:'false'
+    }
+}
+}
+</script>
+
+<style>
+input[type="checkbox"]{
+    display:inline-block;
+}
+</style>
